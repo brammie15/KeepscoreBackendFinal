319fdbe9f7a738e83d6c76d8f1bfe38660321be1
brammie15 <Brammieverhulst@gmail.com>
2023-04-19 02:53:38 +0200

    Add scaffolding, Add CR operation for Klas,Student

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { StudentsModule } from './students/students.module';
 import { KlassenModule } from './klassen/klassen.module';
+import { RubricsModule } from './rubrics/rubrics.module';
+import { CritereasModule } from './critereas/critereas.module';
+import { IndicatorsModule } from './indicators/indicators.module';
 
 @Module({
-  imports: [PrismaModule, StudentsModule, KlassenModule, KlassenModule],
+  imports: [PrismaModule, StudentsModule, KlassenModule, KlassenModule, RubricsModule, CritereasModule, IndicatorsModule],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
